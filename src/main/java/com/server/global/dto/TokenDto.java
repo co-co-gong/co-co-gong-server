6c761b0bff5e6e808af23ba515b18666be719ba5
Zerohertz <ohg3417@gmail.com>
2024-10-30 23:09:07 +0900

    :recycle: refactor: oauth

--- src/main/java/com/server/global/dto/TokenDto.java
+++ src/main/java/com/server/global/dto/TokenDto.java
@@ -1,5 +1,6 @@
 package com.server.global.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,7 +8,13 @@ import lombok.NoArgsConstructor;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "Token data transfer object")
 public class TokenDto {
+
+    @Schema(description = "Access token", example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...")
     private String accessToken;
+
+    @Schema(description = "Refresh token", example = "dGhpcyBpcyBhIHJlZnJlc2ggdG9rZW4...")
     private String refreshToken;
+
 }
