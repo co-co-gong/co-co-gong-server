eb40f301244ed5f47e73bf03e857e72a4c1cfc66
Zerohertz <ohg3417@gmail.com>
2024-10-30 23:01:18 +0900

    :bug: fix: authorize http requests

--- src/main/java/com/server/global/config/SecurityConfig.java
+++ src/main/java/com/server/global/config/SecurityConfig.java
@@ -8,11 +8,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -23,17 +18,17 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login", "/login/**", "/githubLogin/**").permitAll() // 수정된 부분
-                        .requestMatchers("/", "/css/**", "/images/**", "/js/**", "/favicon.ico").permitAll()
-                        .requestMatchers("/api/**", "/api-docs/**", "/swagger/**", "/swagger-ui/**").permitAll()
-                        .requestMatchers("/auth/**", "/oauth2/**", "/signaling/**").permitAll()
-                        .anyRequest().authenticated()
-                )
+                        // Auth
+                        .requestMatchers("/login", "/login/**", "/refresh").permitAll()
+                        // API
+                        .requestMatchers("/api/**").permitAll()
+                        // Swagger
+                        .requestMatchers("/api-docs/**", "/swagger-ui/**", "/swagger/**").permitAll()
+                        .anyRequest().authenticated())
                 .csrf(csrf -> csrf.disable())
                 .httpBasic(httpBasic -> httpBasic.disable())
                 .formLogin(form -> form.disable())
--- src/main/java/com/server/global/error/code/AuthErrorCode.java
+++ src/main/java/com/server/global/error/code/AuthErrorCode.java
@@ -8,7 +8,10 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum AuthErrorCode implements ErrorCode {
-    INVALID_TOKEN(HttpStatus.UNAUTHORIZED.value(), "유효하지 않은 토큰입니다.");
+    URL_NOT_PERMITTED(HttpStatus.FORBIDDEN.value(), "허용되지 않은 URL입니다."),
+    OAUTH_PROCESS_ERROR(HttpStatus.INTERNAL_SERVER_ERROR.value(), "OAuth 과정 중 오류가 발생했습니다."),
+    INVALID_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED.value(), "유효하지 않은 access token입니다."),
+    INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED.value(), "유효하지 않은 refresh token입니다.");
 
     private final int status;
     private final String message;
