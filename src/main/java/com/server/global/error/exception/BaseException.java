35da7119180c5844b65494ab9e8f4b6483164be5
Zerohertz <ohg3417@gmail.com>
2025-01-07 21:41:00 +0900

    :recycle: add: error code

--- src/main/java/com/server/global/error/exception/BaseException.java
+++ src/main/java/com/server/global/error/exception/BaseException.java
@@ -6,10 +6,12 @@ import lombok.Getter;
 
 @Getter
 public class BaseException extends RuntimeException {
+    private final ErrorCode errorCode;
     private final int status;
     private final String message;
 
     public BaseException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
         this.status = errorCode.getStatus();
         this.message = errorCode.getMessage();
     }
