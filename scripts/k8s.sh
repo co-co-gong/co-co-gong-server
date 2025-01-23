e58b865c08dcbc6406e96647d654bd8bf8c7e4a0
Zerohertz <ohg3417@gmail.com>
2025-01-23 20:59:25 +0900

    :hammer: refactor: k8s manifests

--- scripts/k8s.sh
+++ scripts/k8s.sh
@@ -2,6 +2,6 @@
 
 kubectl delete ns co-co-gong
 kubectl create ns co-co-gong
-kubectl apply -n co-co-gong -f k8s
+kubectl apply -n co-co-gong -f k8s/prod
 
 # kubectl exec -it -n co-co-gong deploy/backend -- zsh
